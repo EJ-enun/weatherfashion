bf986e6956a146007ffaf6a41f6d9f8c4d40c7d3
EJay Enun <68190790+EJ-enun@users.noreply.github.com>
2024-05-19 19:22:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ import base64
 import io
 from io import BytesIO
 from opencage.geocoder import OpenCageGeocode
-from transformers import pipeline
+from transformers import pipeline, AutoModelForCausalLM
 
+model = AutoModelForCausalLM.from_pretrained("Snowflake/snowflake-arctic-instruct", trust_remote_code=True)
 
 
 # API key from: https://opencagedata.com
@@ -186,10 +187,10 @@ def get_location(address):
         	st.write('Location not found')
        	return st.json(consumeOne(fetchForecast(lat, lng, API_WEATHER)))
 # Load the text-to-caption model
-model = pipeline("text-generation", model="Snowflake/snowflake-arctic-instruct", trust_remote_code=True)
+#model = pipeline("text-generation", model="Snowflake/snowflake-arctic-instruct", trust_remote_code=True)
 
 # Load the image-to-text model
-model = pipeline('image-to-text', model='ydshieh/vit-gpt2-coco-en')
+model_ydshieh = pipeline('image-to-text', model='ydshieh/vit-gpt2-coco-en')
 
 def main():
     image_url = "https://raw.githubusercontent.com/EJ-enun/weatherfashion/main/OIG.jpg"
