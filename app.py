edf55cd9de234196a377f6b704e40cc30de3bfce
EJay Enun <68190790+EJ-enun@users.noreply.github.com>
2024-05-20 18:48:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -271,7 +271,7 @@ def main():
   if uploaded_file is not None:
     image = Image.open(uploaded_file)
     if image.mode == 'RGBA':
-      #image = image.convert('RGB')
+       image = image.convert('RGB')
     st.image(image, caption="Uploaded Image", use_column_width=True)
     if st.button('Generate'):
         buffered = io.BytesIO()
