3ce6212c25188a20b79f268bed0b9353199f11e2
EJay Enun <68190790+EJ-enun@users.noreply.github.com>
2024-05-20 18:41:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -219,9 +219,7 @@ def generate_arctic_response():
 
 
 def get_blip_output(input):
-   input = {
-    "image": "https://replicate.delivery/mgxm/f4e50a7b-e8ca-432f-8e68-082034ebcc70/demo.jpg"
-}
+   
 
    output = replicate.run(
     "salesforce/blip:2e1dddc8621f72155f24cf2e0adbde548458d3cab9f00c0139eea840d0ac4746",
