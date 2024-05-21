40d3b362658a351e79ee9bf956135fbafa38db61
EJay Enun <68190790+EJ-enun@users.noreply.github.com>
2024-05-21 19:11:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -236,22 +236,21 @@ def image_captions(temp, top_p):
     out_blip = None
     if st.button('Generate'):
         out_blip = get_blip_output(img)
-
-    for event in replicate.stream(
-      "snowflake/snowflake-arctic-instruct",
-      input={
-          "top_k": 50,
-          "top_p": top_p,
-          "prompt": f" write a fun caption about {out_blip}",
-          "temperature": temp,
-          "max_new_tokens": 512,
-          "min_new_tokens": 0,
-          "stop_sequences": "<|im_end|>",
-          "prompt_template": "<|im_start|>system\nYou're a helpful assistant<|im_end|>\n<|im_start|>user\n{prompt}<|im_end|>\n\n<|im_start|>assistant\n",
-          "presence_penalty": 1.15,
-          "frequency_penalty": 0.2
-      },
-  ):st.write(str(event), end="")
+        for event in replicate.stream(
+          "snowflake/snowflake-arctic-instruct",
+          input={
+              "top_k": 50,
+              "top_p": top_p,
+              "prompt": f" write a fun caption about {out_blip}",
+              "temperature": temp,
+              "max_new_tokens": 512,
+              "min_new_tokens": 0,
+              "stop_sequences": "<|im_end|>",
+              "prompt_template": "<|im_start|>system\nYou're a helpful assistant<|im_end|>\n<|im_start|>user\n{prompt}<|im_end|>\n\n<|im_start|>assistant\n",
+              "presence_penalty": 1.15,
+              "frequency_penalty": 0.2
+          },
+      ):st.write(str(event), end="")
   
 
 def reset_app():
